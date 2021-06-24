318ed031a5bf6c2f65137bf3e3757418b9f4f836
luffingluffy <luffingluffy@gmail.com>
2021-06-24 22:39:48 +0800

    added onboarding

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet, Image} from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+const Dots = ({selected}) => {
+    let backgroundColor;
+
+    backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+
+    return (
+        <View
+            style={{
+                width: 5,
+                height: 5,
+                marginHorizontal: 3,
+                backgroundColor
+            }}
+        />
+    );
+}
+
+const Skip = ({... props}) => (
+    <Button
+        title={'Skip'}
+        color={'#000000'}
+        {... props}
+    />
+);
+
+const Next = ({... props}) => (
+    <Button
+        title={'Next'}
+        color={'#000000'}
+        {... props}
+    />
+);
+
+const Done = ({... props}) => (
+    <Button
+        title={'Done'}
+        color={'#000000'}
+        {... props}
+    />
+);
+
+const OnboardingScreen = ({navigation}) => {
+  return (
+      <Onboarding
+          SkipButtonComponent={Skip}
+          NextButtonComponent={Next}
+          DoneButtonComponent={Done}
+          DotsComponent={Dots}
+          onSkip={() => navigation.navigate("Login")}
+          onDone={() => navigation.navigate("Login")}
+          pages={[
+              {
+                  backgroundColor: '#a6e4d0',
+                  image: <Image source={require('../assets/onboarding-img1.png')}/>,
+                  title: 'Welcome to FixMyLife!',
+                  subtitle: 'Are You Ready to Change Your Life?',
+              },
+              {
+                  backgroundColor: '#fdeb93',
+                  image: <Image source={require('../assets/onboarding-img2.png')}/>,
+                  title: 'Onboarding 2',
+                  subtitle: 'Done with React Native Onboarding Swiper',
+              },
+          ]}
+      />
+  );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
