f38d1da510ceace5f8b56bc91b74ff0768f17609
luffingluffy <luffingluffy@gmail.com>
2021-06-26 08:47:08 +0800

    added task components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,30 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import FormButton from '../components/FormButton';
 import {AuthContext} from '../navigation/AuthProvider';
+import Tasks from '../components/Tasks'
 
 const HomeScreen = () => {
+    const [task, setTask] = useState();
+    const [taskItems, setTaskItems] = useState([]);
+
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTask(null);
+    }
+
+    const completeTask = (index) => {
+        let itemsCopy = [...taskItems];
+        itemsCopy.splice(index, 1);
+        setTaskItems(itemsCopy)
+    }
+
     const {user, logout} = useContext(AuthContext)
     return (
         <View style={styles.container}>
             <Text style={styles.text}>
-                Welcome {user.uid}
+                Welcome {user.email}
             </Text>
             <FormButton buttonTitle="Logout" onPress = {() => logout()} />
         </View>
@@ -25,6 +41,45 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 20,
     },
+    tasksWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    items: {
+        marginTop: 30,
+    },
+    writeTaskWrapper: {
+        position: 'absolute',
+        bottom: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    input: {
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+        width: 250,
+    },
+    addWrapper: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+    },
+    addText: {},
     text: {
         fontSize: 20,
         color: '#333333'
