114cf435081a3dc91c1b6436d166817b7a5ab57a
luffingluffy <luffingluffy@gmail.com>
2021-06-28 19:56:23 +0800

    implemented task add/delete

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -7,7 +7,22 @@ const Stack = createStackNavigator();
 const AppStack = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen}/>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{
+                    title: 'FixMyLife',
+                    headerStyle: {
+                        backgroundColor: '#000743'
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontSize: 30,
+                        fontWeight: 'bold',
+                        fontFamily: 'HeadlinerNo.45'
+                    },
+                }}
+            />
         </Stack.Navigator>
     );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React, {useContext, useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import FormButton from '../components/FormButton';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Keyboard,
+    ScrollView,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform, TextInput,
+} from 'react-native';
 import {AuthContext} from '../navigation/AuthProvider';
-import Tasks from '../components/Tasks'
+import Task from '../components/Tasks';
 
 const HomeScreen = () => {
     const [task, setTask] = useState();
@@ -23,10 +31,45 @@ const HomeScreen = () => {
     const {user, logout} = useContext(AuthContext)
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>
-                Welcome {user.email}
-            </Text>
-            <FormButton buttonTitle="Logout" onPress = {() => logout()} />
+            <ScrollView
+                contentContainerStyle={{
+                    flexGrow: 1
+                }}
+                keyboardShouldPersistTaps='handled'
+            >
+
+                <View style={styles.tasksWrapper}>
+                    <Text style={styles.sectionTitle}>Tasks</Text>
+                    <View style={styles.items}>
+                        {
+                            taskItems.map((item, index) => {
+                                return (
+                                    <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                                        <Task text={item} />
+                                    </TouchableOpacity>
+                                )
+                            })
+                        }
+                    </View>
+                </View>
+            </ScrollView>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.writeTaskWrapper}
+            >
+                <TextInput style={styles.input}
+                           placeholder={'Write a task'}
+                           value={task}
+                           onChangeText={text => setTask(text)}
+                           autoCapitalize="none"
+                           autoCorrect={false}
+                />
+                <TouchableOpacity onPress={() => handleAddTask()}>
+                    <View style={styles.addWrapper}>
+                        <Text style={styles.addText}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
         </View>
     );
 }
@@ -35,18 +78,15 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#f9fafd',
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
+        backgroundColor: '#E8EAED',
     },
     tasksWrapper: {
-        paddingTop: 80,
+        paddingTop: 30,
         paddingHorizontal: 20,
     },
     sectionTitle: {
-        fontSize: 24,
+        fontSize: 35,
         fontWeight: 'bold'
     },
     items: {
@@ -55,6 +95,7 @@ const styles = StyleSheet.create({
     writeTaskWrapper: {
         position: 'absolute',
         bottom: 60,
+        left: 15,
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'space-around',
@@ -64,10 +105,10 @@ const styles = StyleSheet.create({
         paddingVertical: 15,
         paddingHorizontal: 15,
         backgroundColor: '#FFF',
-        borderRadius: 60,
+        borderRadius: 25,
         borderColor: '#C0C0C0',
         borderWidth: 1,
-        width: 250,
+        width: 260,
     },
     addWrapper: {
         width: 60,
@@ -78,10 +119,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderColor: '#C0C0C0',
         borderWidth: 1,
+        marginHorizontal: 30
     },
     addText: {},
-    text: {
-        fontSize: 20,
-        color: '#333333'
-    }
 });
